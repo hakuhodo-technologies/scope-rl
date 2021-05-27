8472ec49f96b3c756393544dec27cc4fd0961f88
aiueola <haruharu10540@gmail.com>
2021-05-27 15:22:28 +0900

    black

--- _gym/simulator/base.py
+++ _gym/simulator/base.py
@@ -2,7 +2,8 @@
 from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass
 from typing import Tuple
-from nptyping import NDArray
+
+import numpy as np
 
 
 @dataclass
@@ -14,48 +15,46 @@ class BaseSimulator(metaclass=ABCMeta):
         self,
         timestep: int,
         adjust_rate: int,
-        ad_ids: NDArray[int],
-        user_ids: NDArray[int],
-    ) -> Tuple[NDArray[int]]:
+        ad_ids: np.ndarray,
+        user_ids: np.ndarray,
+    ) -> Tuple[np.ndarray]:
         """Simulate bidding auction for given queries and return outcome."""
         raise NotImplementedError
 
     @abstractmethod
     def fit_reward_predictor(self, n_samples: int) -> None:
         """Fit reward predictor in advance to use prediction in bidding price determination."""
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
-    def _predict_reward(
-        self, timestep: int, contexts: NDArray[float]
-    ) -> NDArray[float]:
+    def _predict_reward(self, timestep: int, contexts: np.ndarray) -> np.ndarray:
         """Predict reward (i.e., auction outcome) to determine bidding price."""
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def _calc_ground_truth_reward(
-        self, timestep: int, contexts: NDArray[float]
-    ) -> NDArray[float]:
+        self, timestep: int, contexts: np.ndarray
+    ) -> np.ndarray:
         """Calculate ground-truth reward (i.e., auction outcome) to determine bidding price."""
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def _map_idx_to_contexts(
-        self, ad_ids: NDArray[int], user_ids: NDArray[int]
-    ) -> NDArray[float]:
+        self, ad_ids: np.ndarray, user_ids: np.ndarray
+    ) -> np.ndarray:
         """Map the ad and the user index into context vectors."""
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def _determine_bid_price(
-        self, timestep: int, adjust_rate: int, contexts: NDArray[float]
-    ) -> NDArray[int]:
+        self, timestep: int, adjust_rate: int, contexts: np.ndarray
+    ) -> np.ndarray:
         """Determine the bidding price using given adjust rate and the predicted/ground-truth rewards."""
-        raise NotImplementedError
+        raise NotImplementedError()
 
     @abstractmethod
     def _calc_and_sample_outcome(
-        self, timestep: int, bid_prices: NDArray[int], contexts: NDArray[float]
-    ) -> Tuple[NDArray[float]]:
+        self, timestep: int, bid_prices: np.ndarray, contexts: np.ndarray
+    ) -> Tuple[np.ndarray]:
         """Calculate pre-determined probabilities from contexts and stochastically sample the outcome."""
-        raise NotImplementedError
+        raise NotImplementedError()
