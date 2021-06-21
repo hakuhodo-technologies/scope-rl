4fd8dcc46731ee94fdebec1a3cf25ad035a47621
aiueola <haruharu10540@gmail.com>
2021-06-21 16:22:51 +0900

    minor fix

--- tests/env/simulator/test_rtb_synthetic.py
+++ tests/env/simulator/test_rtb_synthetic.py
@@ -1,7 +1,6 @@
 import pytest
 
 import numpy as np
-from sklearn.linear_model import LogisticRegression
 
 from _gym.utils import NormalDistribution
 from _gym.env.simulator.rtb_synthetic import RTBSyntheticSimulator
