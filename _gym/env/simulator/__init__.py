7cc94c28265aec23f63f5e9fb07894c76ef5dc9f
aiueola <haruharu10540@gmail.com>
2021-11-02 16:58:19 +0900

    WinningPriceFunction, CTR, CVR

---
+++ _gym/env/simulator/__init__.py
@@ -0,0 +1,8 @@
+from _gym.env.simulator.function import BaseWinningPriceDistribution
+from _gym.env.simulator.function import BaseClickAndConversionRate
+
+
+__all__ = [
+    "BaseWinningPriceDistribution",
+    "BaseClickAndConversionRate",
+]
