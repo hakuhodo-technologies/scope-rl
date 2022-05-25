fa30e373aea1e18fd478a36c1fa2a16db0e7e3bb
aiueola <haruharu10540@gmail.com>
2022-05-25 16:08:10 +0900

    refactor and registrarion

--- rtbgym/__init__.py
+++ rtbgym/__init__.py
@@ -1,6 +1,8 @@
-from rtbgym.env.rtb import RTBEnv
-from rtbgym.env.wrapper_rtb import CustomizedRTBEnv
-from rtbgym.env.simulator.function import (
+import gym
+
+from rtbgym.envs.rtb import RTBEnv
+from rtbgym.envs.wrapper_rtb import CustomizedRTBEnv
+from rtbgym.envs.simulator.function import (
     BaseWinningPriceDistribution,
     BaseClickAndConversionRate,
 )
@@ -12,3 +14,21 @@ __all__ = [
     "BaseWinningPriceDistribution",
     "BaseClickAndConversionRate",
 ]
+
+
+# register standard environment
+env = RTBEnv(random_state=12345)
+# discrete environment
+gym.envs.register(
+    id="RTBEnv-discrete-v0",
+    entry_point="rtbgym.envs.wrapper_rtb:CustomizedRTBEnv",
+    max_episode_steps=7,
+    kwargs={"original_env": env, "action_type": "discrete"},
+)
+# continuous environment
+gym.envs.register(
+    id="RTBEnv-continuous-v0",
+    entry_point="rtbgym.envs.wrapper_rtb:CustomizedRTBEnv",
+    max_episode_steps=7,
+    kwargs={"original_env": env, "action_type": "continuous"},
+)
