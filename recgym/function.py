797c1424bfc23a8a95cbfe8d90a921839d0ca35e
kishi <kishimoto1ren@gmail.com>
2023-01-09 23:37:56 +0900

    update: rec_customize, rec_data_collection

--- recgym/function.py
+++ recgym/function.py
@@ -1,14 +1,14 @@
 
 import numpy as np
 
-#実際のuser_transitionの関数、今回はuser_preference_dynamicsを使う
+#user_transition_function
 def user_preference_dynamics(state, action, item_feature_vector, alpha = 1):
     state = state + alpha * state @ item_feature_vector[action] * item_feature_vector[action] 
     state = state / np.linalg.norm(state, ord=2)
     return state
 
 
-#rewardの関数を決定する
+#reward_function
 def inner_reward_function(state, action, item_feature_vector):
     reward = state @ item_feature_vector[action]
     return reward
--- recgym/rec.py
+++ recgym/rec.py
@@ -13,24 +13,15 @@ class RECEnv(gym.Env):
     Note
     -------
     Adopt OpenAI Gym like interface. See Examples below for the usage.
-    後で
+    
     Markov Decision Process (MDP) definition are given as follows:
-        state: array-like of shape (7, )
-            Statistical feedbacks of auctions during the timestep, including following values.
-                - timestep
-                - remaining budget
-                - impression level features at the previous timestep
-                  (budget consumption rate, cost per mille of impressions, auction winning rate, and reward)
-                - adjust rate (i.e., RL agent action) at the previous timestep
-        action: {int, float, array-like of shape (1, )} (>= 0)
-            Adjust rate parameter used for determining the bid price as follows.
-            (Bid price is individually determined for each auction.)
-            .. math::
-                {bid price}_{t, i} = {adjust rate}_{t} \\times {predicted reward}_{t,i} ( \\times {const.})
-            Note that, you can also use predicted reward instead of ground-truth reward in the above equation.
-            Please also refer to CustomizedRTBEnv Wrapper.
-        reward: int (>= 0)
-            Total clicks/conversions gained during the timestep.
+        state: array-like of shape (user_feature_dim, )
+            make the initial state the user_feature of the chosen user
+            update state with state_transition_function
+        action: {int, array-like of shape (1, )} (>= 0)
+            selected from n_items
+        reward: float 
+            rewards are determined by reward_function
 
     Parameters
     -------
@@ -47,39 +38,40 @@ class RECEnv(gym.Env):
     user_feature_vector: ndarray of shape (n_users, user_feature_dim), default=None
         Feature vectors that characterizes each user.
     reward_function: default = user_preference_dynamics
-        select reward_function
+        Functions that determine how reward is calculated
     state_transition_function: default = inner_reward_function
-        select state_transition_function
+        Functions that determine state update
     noise_std: float, default = 0 (>=0)
+        Amount of noise an observation has
 
-    後で
     Examples
     -------
     .. codeblock:: python
-        # import necessary module from rtbgym
-        from rtbgym.env import RTBEnv
-        from rtbgym.policy import OnlineHead
-        from rtbgym.ope.online import calc_on_policy_policy_value
+        # import necessary module from recgym
+        from recgym.rec import RECEnv
+        from recgym.function import inner_reward_function
+        from recgym.function import user_preference_dynamics
         # import necessary module from other libraries
-        from d3rlpy.algos import RandomPolicy
-        from d3rlpy.preprocessing import MinMaxActionScaler
+        from d3rlpy.algos import DiscreteRandomPolicy
         # initialize environment and define (RL) agent (i.e., policy)
-        env = RTBEnv(random_state=12345)
-        agent = OnlineHead(
-            RandomPolicy(
-                action_scaler=MinMaxActionScaler(
-                    minimum=0.1,
-                    maximum=10,
-                )
-            )
+        env = RECEnv(reward_function = inner_reward_function, state_transition_function = user_preference_dynamics,)
+        agent = DiscreteEpsilonGreedyHead(
+            # base_policy = ddqn,
+            base_policy = DiscreteRandomPolicy(),
+            name = 'random',
+            n_actions = env.n_items,
+            epsilon = 1. ,
+            random_state = random_state, 
         )
         # OpenAI Gym like interaction with agent
         for episode in range(1000):
             obs = env.reset()
             done = False
             while not done:
-                action = agent.predict_online(obs)
-                obs, reward, done, info = env.step(action)
+                action = .sample_action_online(obs)
+                obs, reward, done, truncated, info = env.step(action)
+
+
         # calculate on-policy policy value
         on_policy_performance = calc_on_policy_policy_value(
             env,
@@ -88,6 +80,8 @@ class RECEnv(gym.Env):
             random_state=12345
         )
         on_policy_performance  # 13.44
+
+
     References
     -------
     Takuma Seno and Michita Imai.
@@ -106,8 +100,6 @@ class RECEnv(gym.Env):
         user_feature_dim: int = 5,
         item_feature_vector: Optional[np.ndarray] = None,
         user_feature_vector: Optional[np.ndarray] = None,
-        # reward_function = inner_reward_function,
-        # state_transition_function = user_preference_dynamics,
         noise_std: float = 0,
         step_per_episode = 10,
     ):
@@ -122,10 +114,12 @@ class RECEnv(gym.Env):
         self.noise_std = noise_std
         self.step_per_episode = step_per_episode
 
-        #ユーザーの特徴ベクトルを初期化する
-        user_feature_vector = np.random.uniform(low=-1.0, high=1.0, size=(self.n_users, self.user_feature_dim))
-        #アイテムの特徴ベクトルを初期化する
-        item_feature_vector = np.random.uniform(low=-1.0, high=1.0, size=(self.n_items, self.item_feature_dim))
+        #initialize user_feature_vector
+        if user_feature_vector is None:
+            user_feature_vector = np.random.uniform(low=-1.0, high=1.0, size=(self.n_users, self.user_feature_dim))
+        #initialize item_feature_vector
+        if item_feature_vector is None:
+            item_feature_vector = np.random.uniform(low=-1.0, high=1.0, size=(self.n_items, self.item_feature_dim))
 
         self.item_feature_vector = item_feature_vector
         self.user_feature_vector = user_feature_vector
@@ -140,7 +134,6 @@ class RECEnv(gym.Env):
         # define action space 
         self.action_type = "discrete"
         self.action_dim = 1
-        # self.action_space = Discrete(self.n_items-1, dtype=int)
         self.action_space = Discrete(self.n_items - 1 )
 
         # define reward range
@@ -148,79 +141,64 @@ class RECEnv(gym.Env):
 
     
     def _observation(self, state):
-        #stateにノイズを加えたものを観測とする
+        #add noise to state
         obs = state + np.random.normal(loc=0.0, scale=self.noise_std, size=self.user_feature_dim)
-        #-1.0から1.0に収まるように制限する
+        #limit observation [-1, 1]
         obs = np.clip(obs, -1.0, 1.0)
         return obs
 
 
     def step(
       self,
-      action: int,  # selected item_feature_vector
-    #   action: np.ndarray,  # selected item_feature_vector
+      action: int,   #action: np.ndarray,  # selected from n_items
     ):
-        """Rollout auctions arise during the timestep and return feedbacks to the agent.
+        """return feedbacks to the agent.
 
         Note
         -------
         The rollout procedure is given as follows.
-        1. Sample ads and users for (search volume, ) auctions occur during the timestep. (in Simulator)
-        2. Determine bid price. (In Bidder)
-            :math:`bid_price_{t, i} = adjust_rate_{t} \\times predicted_reward_{t,i}/ground_truth_reward_{t, i} ( \\times const.)`
-        3. Calculate outcome probability and stochastically determine auction result. (in Simulator)
-            auction results: cost (i.e., second price), impression, click, conversion
-        4. Check if the cumulative cost during the timestep exceeds the remaining budget or not.
-           (If exceeds, cancel the corresponding auction results.)
-        5. Aggregate auction results and return feedbacks to the RL agent.
+        1. update state with state_transition_function
+        
+        2. increase the number of step_per_episode
+        3. generate reward
+        4. return feedbacks to the RL agent.
         Parameters
         -------
-        action: {int, float, array-like of shape (1, )} (>= 0)
-            RL agent action which corresponds to the adjust rate parameter used for bid price calculation.
+        action: {int, array-like of shape (1, )} (>= 0)
+            
         Returns
         -------
         feedbacks: Tuple
             obs: ndarray of shape (1,)
-                        Statistical feedbacks of recommendation during the timestep.
-                        Corresponds to RL state, which include following components.
-                            - state
-            reward: int (>= 0)
-                Total clicks/conversions gained during the timestep.
+                        Statistical feedbacks of recommendation
+                            - add noise to state by _observation()
+            reward: float
             done: bool
                 Wether the episode end or not.
+            truncated: False
             info: dict
                 Additional feedbacks for analysts.
-                    #一旦空で返す
+                #一旦空で返す
         """
 
-
-        # state_transition_functionでstateを更新
+        # update state with state_transition_function
         state = self.state_transition_function(self.state, action, self.item_feature_vector)
-        # state =  eval("self."+ self.state_transition_function+"(self.state, action)")
 
-        #step_per_episodeが決まっている場合の設定   
+
         done = self.t == self.step_per_episode - 1
 
         if done:
-            obs = self.reset()
+            obs, info = self.reset()
 
         else:
             # update timestep
             self.t += 1
             obs = self._observation(state)
-            # obs = [0]*self.user_feature_dim
-            # obs = np.ones(self.user_feature_dim)
-            
 
-
-
-        # 報酬を発生
+        # generate reward
         reward = self.reward_function(state, action, self.item_feature_vector)
-        # reward = eval("self." + self.reward_function +"(self.state, action)")
         
-
         info = {}
-
 
         return obs, reward, done, False, info
         # return obs, reward, done, info
@@ -233,36 +211,19 @@ class RECEnv(gym.Env):
         Returns
         -------
         obs: ndarray of shape (1,)
-                    Statistical feedbacks of recommendation during the timestep.
-                    Corresponds to RL state, which include following components.
-                        - state
+                    Statistical feedbacks of recommendation
+                        - add noise to state by _observation()
         """
         # initialize internal env state
+        # initialize timestep
         self.t = 0
-        #userをランダムに選択
+        #select user at random
         user_id = random.randint(0, self.n_users-1)
-        #user_feature_vectorから選ばれた人のuser_featureをstateにする
+        #make state user_feature_vector of the selected user.
         state = self.user_feature_vector[user_id]
         self.state = state
         obs = self._observation(self.state)
-        # obs = np.array([0]*self.user_feature_dim)
-        # obs = np.ones(self.user_feature_dim)
-        return obs
 
+        info = {}
 
-
-
-#実際のuser_transitionの関数、今回はuser_preference_dynamicsを使う
-def user_preference_dynamics(state, action, item_feature_vector, alpha = 1):
-    state = state + alpha * state @ item_feature_vector[action] * item_feature_vector[action] 
-    state = state / np.linalg.norm(state, ord=2)
-    return state
-
-
-#rewardの関数を決定する
-def inner_reward_function(state, action, item_feature_vector):
-    reward = state @ item_feature_vector[action]
-    return reward
-
-# def cos_similar_function(state, action):
-#     reward = 
+        return obs, info
