c30204b27ae550bad4470cc4e95dbf1e3a031bbe
kishi <kishimoto1ren@gmail.com>
2023-04-21 03:47:52 +0900

    update tutorial

--- tutorial/function/utils.py
+++
@@ -1,6 +0,0 @@
-def format_runtime(start: int, finish: int):
-    runtime = finish - start
-    hour = int(runtime // 3600)
-    min = int((runtime // 60) % 60)
-    sec = int(runtime % 60)
-    return f"{hour}h.{min}m.{sec}s"
