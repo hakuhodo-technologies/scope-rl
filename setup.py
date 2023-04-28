35a451e6081c44b8585f55495d3eb64c7aaf8368
aiueola <haruharu10540@gmail.com>
2023-04-28 11:53:21 +0900

    stash

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 
 # setup OFRL
-setup(name="ofrl", version="0.0.0", install_requires=["gym"], py_modules=['ofrl'])
+setup(name="ofrl", version="0.0.0", install_requires=["gym"], py_modules=["ofrl"])
 # setup RTBEnv
-setup(name="rtbgym", version="0.0.0", install_requires=["gym"], py_modules=['rtbgym'])
+setup(name="rtbgym", version="0.0.0", install_requires=["gym"], py_modules=["rtbgym"])
