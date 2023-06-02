88a287180e768cc6e1edf616ea132e2b27bbd6df
aiueola <haruharu10540@gmail.com>
2023-06-02 13:06:17 +0900

    fix imports

--- recgym/envs/simulator/__init__ .py
+++ recgym/envs/simulator/__init__ .py
@@ -1,6 +1,10 @@
 # Copyright (c) 2023, Haruka Kiyohara, Ren Kishimoto, HAKUHODO Technologies Inc., and Hanjuku-kaso Co., Ltd. All rights reserved.
 # Licensed under the Apache 2.0 License.
 
+from recgym.envs.simulator.base import BaseUserModel
 from recgym.envs.simulator.function import UserModel
 
-__all__ = ["UserModel"]
+__all__ = [
+    "BaseUserModel",
+    "UserModel",
+]
