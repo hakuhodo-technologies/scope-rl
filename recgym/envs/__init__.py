b21da64b54c5d60fca06f837cb83a33af5be2adc
aiueola <haruharu10540@gmail.com>
2023-06-02 12:46:55 +0900

    add __init__.py

--- recgym/envs/__init__.py
+++ recgym/envs/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2023, Haruka Kiyohara, Ren Kishimoto, HAKUHODO Technologies Inc., and Hanjuku-kaso Co., Ltd. All rights reserved.
 # Licensed under the Apache 2.0 License.
 
-from basicgym.envs.synthetic import BasicEnv
+from recgym.envs.rec import RECEnv
 
 
 __all__ = [
-    "BasicEnv",
+    "RECEnv",
 ]
--- rtbgym/envs/__init__.py
+++ rtbgym/envs/__init__.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2023, Haruka Kiyohara, Ren Kishimoto, HAKUHODO Technologies Inc., and Hanjuku-kaso Co., Ltd. All rights reserved.
 # Licensed under the Apache 2.0 License.
 
-from basicgym.envs.synthetic import BasicEnv
+from rtbgym.envs.rtb import RTBEnv
+from rtbgym.envs.wrapper_rtb import CustomizedRTBEnv
 
 
 __all__ = [
-    "BasicEnv",
+    "RTBEnv",
+    "CustomizedRTBEnv",
 ]
