57e9704be54810b5f649ee5d37a0761e19adf584
aiueola <haruharu10540@gmail.com>
2023-06-16 08:59:45 +0900

    poetry setting, but failed due to PEP 517 support on d3rlpy

--- setup.py
+++ setup.py
@@ -38,8 +38,6 @@ setup(
     ),
     classifiers=[
         "Intended Audience :: Science/Research",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
