72b16a6a53039f75c190c7a298298551ca1f36eb
aiueola <haruharu10540@gmail.com>
2023-07-24 07:32:06 +0900

    upgrade to scope-rl==0.1.3

--- scope_rl/version.py
+++ scope_rl/version.py
@@ -1,4 +1,4 @@
 # Copyright (c) 2023, Haruka Kiyohara, Ren Kishimoto, HAKUHODO Technologies Inc., and Hanjuku-kaso Co., Ltd. All rights reserved.
 # Licensed under the Apache 2.0 License.
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
