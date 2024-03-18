eabacdd8710682457c2b13a694745cd281b27e0c
Alexandros <alexpatsanis>
2024-03-18 15:00:17 +0100

    Removing Uncessary self in a variable
    self.behavior_policy existis only in this point and it is uncessary. Rename the variable self.behavior_policy to just behavior_policy will work.

--- scope_rl/dataset/synthetic.py
+++ scope_rl/dataset/synthetic.py
@@ -750,7 +750,7 @@ class SyntheticDataset(BaseDataset):
                 (
                     action,
                     action_prob,
-                ) = self.behavior_policy.sample_action_and_output_pscore_online(state)
+                ) = behavior_policy.sample_action_and_output_pscore_online(state)
                 next_state, reward, done, truncated, info_ = self.env.step(action)
 
                 states[idx] = state
